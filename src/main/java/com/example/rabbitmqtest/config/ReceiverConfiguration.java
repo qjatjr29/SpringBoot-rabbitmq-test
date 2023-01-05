8d1cb62d163cbe66734d3711447a664657983b5e
beomsic <qjatjr29@khu.ac.kr>
2023-01-05 20:00:54 +0900

    :sparkles: feat: RabbitTemplate Bean 추가

--- src/main/java/com/example/rabbitmqtest/config/ReceiverConfiguration.java
+++ src/main/java/com/example/rabbitmqtest/config/ReceiverConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -108,6 +109,13 @@ public class ReceiverConfiguration {
   @Bean
   public Jackson2JsonMessageConverter getMessageConverter() {
     return new Jackson2JsonMessageConverter();
+  }
+
+  @Bean
+  public RabbitTemplate rabbitTemplate() {
+    RabbitTemplate rabbitTemplate = new RabbitTemplate(getConnectionFactory());
+    rabbitTemplate.setMessageConverter(getMessageConverter());
+    return rabbitTemplate;
   }
 
 }
